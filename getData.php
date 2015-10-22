0539117f317ca85e74709a287931b58af7ae0766
dtg <tagedo74@gmail.com>
2015-10-22 08:33:13 +0300

    +

--- getData.php
+++ getData.php
@@ -4,33 +4,37 @@ require_once __DIR__ . '/classes/Db.php';
 
 $arrActivities = ['development', 'support', 'marketing', 'documents', 'negotiations'];
 
-$currentContactId = 1;
+$currentContactId = 4;
 // для каждого контакта делаем следующее:
 $quantityActivities = rand(10, 600);  // определяем количество активностей
-for ($i = 1; $i <= 1; $i++) {          // для каждой активности делаем следующее:
+for ($i = 1; $i <= $quantityActivities; $i++) {          // для каждой активности делаем следующее:
     $currentAction = rand(0, 4);     // определяем тип активности
-    $currentEmployeeId = rand(0, 30);    // определяем сотрудника
+    $currentEmployeeId = rand(0, 24);    // определяем сотрудника
     $currentDuration = rand(300, 6000);  // длительность активности в секундах
-    $currentCreatedAt = rand(300, 6000);  // начала активности - метка юникс-тайм
+    $currentCreatedAt = rand(1388520000, 1420059600);  // начала активности - метка юникс-тайм
     // проверяем, была ли уже в этот период времени активность у текущего контакта или текущего сотрудника
     // если уже была, то повторно выбираем $currentTimestamp и повторно проверяем
     $sql = 'SELECT * FROM `activities` WHERE (
             (:currentCreatedAt BETWEEN `created_at` AND (`created_at`+`duration`)) OR
             (:currentCreatedAt < `created_at` AND (:currentCreatedAt + :currentDuration) >= `created_at`))
             AND (`contact_id` IN (:currentContactId, :currentEmployeeId) OR `employee_id` IN (:currentContactId, :currentEmployeeId))';
-    $db = new Db();
-    $select = $db->dbSelect($sql, [':currentCreatedAt' => $currentCreatedAt, ':currentDuration' => $currentDuration,
-    ':currentContactId' => $currentContactId, ':currentEmployeeId' => $currentEmployeeId]);
-    if(empty($select)) {
-        $sql = 'INSERT INTO `activities`
-                VALUES (:currentAction, :currentCreatedAt, :currentDuration, :currentContactId, :currentEmployeeId)';
-        $insert = $db->dbExecute($sql, [':currentAction' => $currentAction, ':currentCreatedAt' => $currentCreatedAt,
-            ':currentDuration' => $currentDuration, ':currentContactId' => $currentContactId,
-            ':currentEmployeeId' => $currentEmployeeId]);
-        var_dump($insert);
-    }
-}
 
+
+//echo strtotime('1 January 2015');
+
+$db = new Db();
+$select = $db->dbSelect($sql, [':currentCreatedAt' => $currentCreatedAt, ':currentDuration' => $currentDuration,
+    ':currentContactId' => $currentContactId, ':currentEmployeeId' => $currentEmployeeId]);
+if (empty($select)) {
+    $sql = 'INSERT INTO `activities` ( `action`,  `created_at`, `duration`, `contact_id`, `employee_id`)
+                VALUES (:currentAction, :currentCreatedAt, :currentDuration, :currentContactId, :currentEmployeeId)';
+    $insert = $db->dbExecute($sql, [':currentAction' => $currentAction, ':currentCreatedAt' => $currentCreatedAt,
+        ':currentDuration' => $currentDuration, ':currentContactId' => $currentContactId,
+        ':currentEmployeeId' => $currentEmployeeId]);
+} else {
+    continue;
+}
+}
 //$sql = 'SELECT `id`, CONCAT(`first_name`, SPACE(1), `last_name`) `name`  FROM `contacts` ORDER BY `id`';
 //$db = new Db();
 //$contacts = $db->dbSelect($sql, [':username' => $_GET['user']]);
