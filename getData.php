86988447c8e2869f02a71f85546b2f40aca80a60
dtg <tagedo74@gmail.com>
2015-10-24 00:27:35 +0300

    +

--- getData.php
+++ getData.php
@@ -5,32 +5,16 @@ require_once __DIR__ . '/classes/Db.php';
 $arrActivities = ['development', 'support', 'marketing', 'documents', 'negotiations'];
 $db = new Db();
 
-$sql = 'SELECT c.id, a.action, a.val FROM (
+$sql = 'SELECT CONCAT_WS(\'  \', c.first_name, c.last_name) contact, a.action, a.val FROM (
 SELECT `contact_id`, `action`, SUM(`duration`) `val` FROM `activities`
-        GROUP BY 1, 2 ORDER BY 1, 2) a, contacts c WHERE c.id = a.contact_id AND c.id <> 13';
+        GROUP BY 1, 2 ORDER BY 1, 2) a, contacts c WHERE c.id = a.contact_id';
 $values = $db->dbSelect($sql);
-
 
 $arrResult = [];
 foreach ($values as $value) {
-    $arrResult[$value->id][$value->action] = $value->val;
+    $arrResult[$value->contact][$value->action] = $value->val;
 }
-//
-//$arrResult =
-//    [
-//        'cols' => [
-//            ['id' => 'task', 'label' => 'Employee Name', 'type' => 'string'],
-//            ['id' => 'endDate', 'label' => 'End Date', 'type' => 'number']
-//        ],
-//        'rows' => [
-//            ['c' => ['v' => 'Mike', 'v' => 600]],
-//            ['c' => ['v' => 'Bob', 'v' => 100]],
-//            ['c' => ['v' => 'Alice', 'v' => 200]],
-//            ['c' => ['v' => 'Frank', 'v' => 300]],
-//            ['c' => ['v' => 'Floyd', 'v' => 400]],
-//            ['c' => ['v' => 'Fritz', 'v' => 500]]
-//        ]
-//    ];
+
 
 echo json_encode($arrResult, JSON_UNESCAPED_UNICODE);
 
