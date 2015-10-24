40dab22c2b9058b2bea51acd7a2f4f74b01ea69a
dtg <tagedo74@gmail.com>
2015-10-24 22:44:15 +0300

    +

--- getData.php
+++ getData.php
@@ -5,34 +5,22 @@ $arrResult = [];
 $arrActivities = ['development', 'support', 'marketing', 'documents', 'negotiations'];
 $db = new Db();
 
-$sql = 'SELECT `contact_id`, SUM(`duration`) `val` FROM `activities`
-        WHERE `created_at` BETWEEN :start AND :end
+$sql = 'SELECT `employee_id`, SUM(`duration`) `val` FROM `activities`
+        WHERE `employee_id` <> \'\' AND `created_at` BETWEEN :start AND :end
         GROUP BY 1 ORDER BY 2 DESC';
 $values1 = $db->dbSelect($sql, [':start' => $_GET['start'], ':end' => $_GET['end']]);
 
 foreach ($values1 as $value1) {
-    $sql = 'SELECT (SELECT CONCAT_WS(\'  \', first_name, last_name) FROM contacts WHERE id = :id) `contact`,
+    $sql = 'SELECT (SELECT CONCAT_WS(\'  \', first_name, last_name) FROM contacts WHERE id = :id) `employee`,
             `action`, SUM(`duration`) `val`
             FROM `activities`
-            WHERE `contact_id` = :id and `created_at` BETWEEN :start AND :end
+            WHERE `employee_id` = :id and `created_at` BETWEEN :start AND :end
             GROUP BY 2 ORDER BY 2, 3';
-    $values2 = $db->dbSelect($sql, [':id' => $value1->contact_id, ':start' => $_GET['start'], ':end' => $_GET['end']]);
+    $values2 = $db->dbSelect($sql, [':id' => $value1->employee_id, ':start' => $_GET['start'], ':end' => $_GET['end']]);
     foreach ($values2 as $value2) {
-        $arrResult[$value2->contact][$value2->action] = $value2->val;
+        $arrResult[$value2->employee][$value2->action] = $value2->val;
     }
 }
-
-
-//$sql = 'SELECT CONCAT_WS(\'  \', c.first_name, c.last_name) contact, a.action, a.val FROM (
-//        SELECT `contact_id`, `action`, SUM(`duration`) `val` FROM `activities`
-//        WHERE `created_at` BETWEEN :start AND :end
-//        GROUP BY 1, 2 ORDER BY 1, 2) a, contacts c WHERE c.id = a.contact_id';
-//$values = $db->dbSelect($sql, [':start' => $_GET['start'], ':end' => $_GET['end']]);
-
-
-//foreach ($values2 as $value) {
-//    $arrResult[$value->contact][$value->action] = $value->val;
-//}
 
 
 echo json_encode($arrResult, JSON_UNESCAPED_UNICODE);
