51ad56e80ef48422a7a961282363dad24965acbf
dtg <tagedo74@gmail.com>
2015-10-24 16:04:33 +0300

    +

--- getData.php
+++ getData.php
@@ -6,9 +6,10 @@ $arrActivities = ['development', 'support', 'marketing', 'documents', 'negotiati
 $db = new Db();
 
 $sql = 'SELECT CONCAT_WS(\'  \', c.first_name, c.last_name) contact, a.action, a.val FROM (
-SELECT `contact_id`, `action`, SUM(`duration`) `val` FROM `activities`
+        SELECT `contact_id`, `action`, SUM(`duration`) `val` FROM `activities`
+        WHERE `created_at` BETWEEN :start AND :end
         GROUP BY 1, 2 ORDER BY 1, 2) a, contacts c WHERE c.id = a.contact_id';
-$values = $db->dbSelect($sql);
+$values = $db->dbSelect($sql, [':start' => $_GET['start'], ':end' => $_GET['end']]);
 
 $arrResult = [];
 foreach ($values as $value) {
