c5d29f913c4ad085a70552bd9258c81d419a0f38
dtg <tagedo74@gmail.com>
2015-10-25 18:58:03 +0300

    +

--- getDataByAction.php
+++ getDataByAction.php
@@ -3,9 +3,11 @@ require_once __DIR__ . '/classes/Db.php';
 
 $db = new Db();
 
-$sql = 'SELECT `action_id`, SUM(`duration`) `val` FROM `activities`
-        WHERE `created_at` BETWEEN :start AND :end
-        GROUP BY 1 ORDER BY 2 DESC';
+$sql = 'SELECT action, SUM(duration) val FROM activities
+        WHERE created_at BETWEEN :start AND :end
+        GROUP BY 1 ORDER BY 1';
+
+
 $values = $db->dbSelect($sql, [':start' => $_GET['start'], ':end' => $_GET['end']]);
 
 echo json_encode($values, JSON_UNESCAPED_UNICODE);
